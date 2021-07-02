da62021d3df14f323a30ace124ac8a9dd13fc560
sukruburakcetin <cetin.sukruburak@gmail.com>
2021-07-02 09:45:10 +0300

    pull from new changes

--- seedlingDetectAndSegment/seedlingDetectAndSegment.cpp
+++ seedlingDetectAndSegment/seedlingDetectAndSegment.cpp
@@ -1,27 +1,42 @@
-// seedlingDetectAndSegment.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <seedlingDetector.hpp>
+#include <boost/filesystem.hpp>
+
 using namespace cv;
 using namespace std;
-
+namespace fs = boost::filesystem;
 
 int main()
 {
     std::cout << "------------------------------- Seedling Classifier ----------------------------------\n";
 
+	std::vector<cv::Mat> imageVec;
+	fs::path p("C:\\Users\\HTG_SOFTWARE\\Desktop\\mixed\\");
+	fs::directory_iterator end_itr;
+	// cycle through the directory
+	for (fs::directory_iterator itr(p); itr != end_itr; ++itr) {
+		// If it's not a directory, list it. If you want to list directories too, just remove this check.
+		if (fs::is_regular_file(itr->path())) {
+			if (fs::is_regular_file(itr->path())) {
+				cv::Mat img;
+				img = cv::imread(itr->path().string());
+				if (img.data) imageVec.push_back(img);
+			}
+			// assign current file name to current_file and echo it out to the console.
+			std::string current_file = itr->path().string();
+			std::cout << current_file << std::endl;
+			Mat src = imread(current_file, IMREAD_COLOR);
+			Mat dst;
+
+			seedlingDetectorPreferences prefs;
+			const seedlingDetectorResult res = seedlingDetector(src, dst, prefs);
+		}
+	}
 	//string image_path = "C:/IMG_3891.png";
-	string image_path = "C:/168.png";
-
-	Mat src = imread(image_path, IMREAD_COLOR);
-	Mat dst;
-
-    seedlingDetectorPreferences prefs;
-    const seedlingDetectorResult res = seedlingDetector(src, dst, prefs);
+	//string image_path = "C:/4.png";
+	//Mat src = imread(image_path, IMREAD_COLOR);
 }
 
 
--- seedlingDetector/seedlingDetector.hpp
+++ seedlingDetector/seedlingDetector.hpp
@@ -1,30 +1,85 @@
-#include <opencv2/core/mat.hpp>
+#include <rapidjson/document.h>
+#include "AbstractResult.hpp"
 
-class seedlingDetectorResult
+
+struct seedlingRoiResult
 {
+	int bodyThickness;
+	int bodyHeight;
+	int leafLength;
+	int seedlingCount;
+	seedlingRoiResult(int bodyThickness = 0, int bodyHeight = 0, int leafLength = 0, int seedlingCount = 0) :
+		bodyThickness(bodyThickness), bodyHeight(bodyHeight), leafLength(leafLength), seedlingCount(seedlingCount)
+	{
+		//std::cerr << "Roi " << " x=" << x << " y=" << y << " h=" << h << " w=" << w << std::endl;
+	}
+};
 
+
+class seedlingDetectorResult : public AbstractResult
+{
 public:
 	virtual ~seedlingDetectorResult() = default;
 
-	int seedlingCount = 0;
+	//int seedlingCount = 0;
+	//std::vector<seedlingRoiResult> roiResults;
+	//int totalBodyHeight;
+	//int totalBodyThickness;
+	//int totalLeafLength;
+	//int totalSeedlingCount;
+	//int averageTotalBodyHeight;
+	//int averageTotalBodyThickness;
+	//int averageTotalLeafLength;
+
+	//rapidjson::Document seedlingDetectorResult::getJsonDoc() override 
+	//{
+	//	rapidjson::Document res;
+	//	res.SetObject();
+	//	rapidjson::Document::AllocatorType& allocator = res.GetAllocator();
+	//	rapidjson::Value rowModelList_array(rapidjson::kArrayType);
+
+	//	for (int i = 0; i < roiResults.size(); i++)
+	//	{
+	//		const seedlingRoiResult roiRes = roiResults[i];
+
+	//		totalBodyHeight = roiRes.bodyHeight;
+	//		totalBodyThickness = roiRes.bodyThickness;
+	//		totalLeafLength = roiRes.leafLength;
+	//		totalSeedlingCount = roiRes.seedlingCount;
+
+	//		rapidjson::Value resultObject(rapidjson::kObjectType);
+
+	//		resultObject.AddMember("bodyHeight", roiRes.bodyHeight, allocator);
+	//		resultObject.AddMember("bodyThickness", roiRes.bodyThickness, allocator);
+	//		resultObject.AddMember("leafLength", roiRes.leafLength, allocator);
+	//		resultObject.AddMember("seedlingCount", roiRes.seedlingCount, allocator);
+
+	//		rowModelList_array.PushBack(resultObject, allocator);
+	//	}
+	//	res.AddMember("rowModelList", rowModelList_array, allocator);
+
+	//	averageTotalBodyHeight = double(totalBodyHeight) / double(totalSeedlingCount);
+	//	averageTotalBodyThickness = double(totalBodyThickness) / double(totalSeedlingCount);
+	//	averageTotalLeafLength = double(totalLeafLength) / double(totalSeedlingCount);
+
+	//	res.AddMember("averageTotalBodyHeight", averageTotalBodyHeight, allocator);
+	//	res.AddMember("averageTotalBodyThickness", averageTotalBodyThickness, allocator);
+	//	res.AddMember("averageTotalLeafLength", averageTotalLeafLength, allocator);
+	//	return res;
+	//}
+
 };
 
 class seedlingDetectorPreferences
 {
-
 public:
 	virtual ~seedlingDetectorPreferences() = default;
-
-
-
 	int vector01 = 650;
 	int vector02 = 704;
 	int vector03 = 286;
 	int vector11 = 268;
 	int vector12 = 570;
 	int vector13 = 776;
-
-
 };
 
 seedlingDetectorResult seedlingDetector(cv::Mat& src, cv::Mat& dst, const seedlingDetectorPreferences& prefs);
