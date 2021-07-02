96ba516b86a232ad0152d56eb7f222e002812c9d
sukruburakcetin <cetin.sukruburak@gmail.com>
2021-07-02 21:17:32 +0300

    small fix

--- seedlingDetector/seedlingDetector.hpp
+++ seedlingDetector/seedlingDetector.hpp
@@ -21,52 +21,52 @@ class seedlingDetectorResult : public AbstractResult
 public:
 	virtual ~seedlingDetectorResult() = default;
 
-	//int seedlingCount = 0;
-	//std::vector<seedlingRoiResult> roiResults;
-	//int totalBodyHeight;
-	//int totalBodyThickness;
-	//int totalLeafLength;
-	//int totalSeedlingCount;
-	//int averageTotalBodyHeight;
-	//int averageTotalBodyThickness;
-	//int averageTotalLeafLength;
+	int seedlingCount = 0;
+	std::vector<seedlingRoiResult> roiResults;
+	int totalBodyHeight;
+	int totalBodyThickness;
+	int totalLeafLength;
+	int totalSeedlingCount;
+	int averageTotalBodyHeight;
+	int averageTotalBodyThickness;
+	int averageTotalLeafLength;
 
-	//rapidjson::Document seedlingDetectorResult::getJsonDoc() override 
-	//{
-	//	rapidjson::Document res;
-	//	res.SetObject();
-	//	rapidjson::Document::AllocatorType& allocator = res.GetAllocator();
-	//	rapidjson::Value rowModelList_array(rapidjson::kArrayType);
+	rapidjson::Document getJsonDoc() override
+	{
+		rapidjson::Document res;
+		res.SetObject();
+		rapidjson::Document::AllocatorType& allocator = res.GetAllocator();
+		rapidjson::Value rowModelList_array(rapidjson::kArrayType);
 
-	//	for (int i = 0; i < roiResults.size(); i++)
-	//	{
-	//		const seedlingRoiResult roiRes = roiResults[i];
+		for (int i = 0; i < roiResults.size(); i++)
+		{
+			const seedlingRoiResult roiRes = roiResults[i];
 
-	//		totalBodyHeight = roiRes.bodyHeight;
-	//		totalBodyThickness = roiRes.bodyThickness;
-	//		totalLeafLength = roiRes.leafLength;
-	//		totalSeedlingCount = roiRes.seedlingCount;
+			totalBodyHeight = roiRes.bodyHeight;
+			totalBodyThickness = roiRes.bodyThickness;
+			totalLeafLength = roiRes.leafLength;
+			totalSeedlingCount = roiRes.seedlingCount;
 
-	//		rapidjson::Value resultObject(rapidjson::kObjectType);
+			rapidjson::Value resultObject(rapidjson::kObjectType);
 
-	//		resultObject.AddMember("bodyHeight", roiRes.bodyHeight, allocator);
-	//		resultObject.AddMember("bodyThickness", roiRes.bodyThickness, allocator);
-	//		resultObject.AddMember("leafLength", roiRes.leafLength, allocator);
-	//		resultObject.AddMember("seedlingCount", roiRes.seedlingCount, allocator);
+			resultObject.AddMember("bodyHeight", roiRes.bodyHeight, allocator);
+			resultObject.AddMember("bodyThickness", roiRes.bodyThickness, allocator);
+			resultObject.AddMember("leafLength", roiRes.leafLength, allocator);
+			resultObject.AddMember("seedlingCount", roiRes.seedlingCount, allocator);
 
-	//		rowModelList_array.PushBack(resultObject, allocator);
-	//	}
-	//	res.AddMember("rowModelList", rowModelList_array, allocator);
+			rowModelList_array.PushBack(resultObject, allocator);
+		}
+		res.AddMember("rowModelList", rowModelList_array, allocator);
 
-	//	averageTotalBodyHeight = double(totalBodyHeight) / double(totalSeedlingCount);
-	//	averageTotalBodyThickness = double(totalBodyThickness) / double(totalSeedlingCount);
-	//	averageTotalLeafLength = double(totalLeafLength) / double(totalSeedlingCount);
+		averageTotalBodyHeight = double(totalBodyHeight) / double(totalSeedlingCount);
+		averageTotalBodyThickness = double(totalBodyThickness) / double(totalSeedlingCount);
+		averageTotalLeafLength = double(totalLeafLength) / double(totalSeedlingCount);
 
-	//	res.AddMember("averageTotalBodyHeight", averageTotalBodyHeight, allocator);
-	//	res.AddMember("averageTotalBodyThickness", averageTotalBodyThickness, allocator);
-	//	res.AddMember("averageTotalLeafLength", averageTotalLeafLength, allocator);
-	//	return res;
-	//}
+		res.AddMember("averageTotalBodyHeight", averageTotalBodyHeight, allocator);
+		res.AddMember("averageTotalBodyThickness", averageTotalBodyThickness, allocator);
+		res.AddMember("averageTotalLeafLength", averageTotalLeafLength, allocator);
+		return res;
+	}
 
 };
 
